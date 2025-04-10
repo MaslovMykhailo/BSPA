5392484fc338f9be65effd4bc8bcbe3f77cb6ccc
Mykhailo Maslov <maslov.mikhail200@gmail.com>
2025-04-11 00:37:12 +0300

    Add starting state, add speed, improve restarting state

--- hw-2-snake/snake.ts
+++ hw-2-snake/snake.ts
@@ -1,5 +1,9 @@
 const GRID_SIZE = 10;
 
+const MIN_SPEED = 300;
+const MAX_SPEED = 50;
+const SPEED_STEP = 10;
+
 type SnakeCell = [number, number];
 type Snake = Array<SnakeCell>;
 
@@ -9,6 +13,7 @@ class SnakeGame extends LitElement {
   static override styles = css`
     :host {
       display: inline-block;
+      position: relative;
       margin: auto;
     }
 
@@ -22,12 +27,34 @@ class SnakeGame extends LitElement {
     table[blurry] {
       filter: blur(4px);
     }
+
+    section {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border-radius: 8px;
+      backdrop-filter: blur(4px);
+      background-color: rgb(from lightgreen r g b / 0.4);
+    }
+
+    section[gameover] {
+      background-color: rgb(from red r g b / 0.4);
+      backdrop-filter: blur(4px);
+    }
   `;
 
   static override get properties() {
     return {
-      gameOver: { type: Boolean, state: true, reflect: true },
-      score: { type: Number, reflect: true },
+      gameStarted: { type: Boolean, state: true },
+      gameOver: { type: Boolean, state: true },
+      score: { type: Number },
+      speed: { type: Number },
     }
   }
 
@@ -106,7 +133,7 @@ class SnakeGame extends LitElement {
 
     if (gameOver) {
       console.log('Game over');
-      clearTimeout(this.interval);
+      clearInterval(this.interval);
     }
 
     snake.push(newHead);
@@ -120,6 +147,7 @@ class SnakeGame extends LitElement {
         for (const [x, y] of snake) {
           if (x === fruitX || y === fruitY) {
             bad = true;
+            console.log({bad})
             break;
           }
         }
@@ -129,6 +157,11 @@ class SnakeGame extends LitElement {
           break;
         }
       }
+
+      this.speed = Math.max(this.speed - SPEED_STEP, MAX_SPEED);
+
+      clearInterval(this.interval);
+      this.interval = setInterval(this.tick.bind(this), this.speed);
     } else {
       snake.shift();
     }
@@ -139,13 +172,16 @@ class SnakeGame extends LitElement {
   }
 
   private direction: 'ArrowUp' | 'ArrowRight' | 'ArrowDown' | 'ArrowLeft' =
-    'ArrowRight';
+    'ArrowUp';
 
   tick() {
     this.gameOver = this.moveSnake(this.snake, this.direction);
-    if (this.gameOver) return;
+    
     this.clear();
     this.drawSnake(this.snake);
+
+    if (this.gameOver) return;
+  
     this.draw(...this.fruit, 'red');
   }
 
@@ -167,15 +203,17 @@ class SnakeGame extends LitElement {
 
   private interval: number | undefined;
 
-  gameOver: boolean = false;
+  private gameStarted: boolean = false;
 
-  score: number = this.snake.length;
+  private gameOver: boolean = false;
 
-  override firstUpdated() {
-    super.firstUpdated(new Map());
-    this.interval = setInterval(this.tick.bind(this), 200);
-    this.drawSnake(this.snake);
+  get gameActive() {
+    return this.gameStarted && !this.gameOver;
   }
+
+  private score: number = this.snake.length;
+
+  private speed: number = MIN_SPEED;
 
   makeRow(cols: number) {
     const cells = new Array(cols).fill(0).map((_) => html`<td></td>`);
@@ -186,9 +224,40 @@ class SnakeGame extends LitElement {
 
   makeTable(rows: number, cols: number) {
     return new Array(rows).fill(0).map((_) => this.makeRow(cols));
-    // return html`<table>
-    //   ${tableRows}
-    // </table>`;
+  }
+  
+  makeScore() {
+    return html`
+      <p>Score: ${this.score} | Speed: ${this.speed}</p>
+    `;
+  }
+
+  makeMenu() {
+    if (this.gameOver) {
+      return html`
+        <section gameover>
+          <h3>Game over</h3>
+          ${this.makeScore()}
+          <button @click=${this.restartGame}>Restart</button>
+        </section>
+      `
+    }
+
+    if (!this.gameStarted) {
+      return html`
+        <section>
+          <button @click=${this.startGame}>Start</button>
+        </section>
+      `;
+    }
+
+    return nothing
+  }
+
+  startGame() {
+    this.gameStarted = true;
+    this.interval = setInterval(this.tick.bind(this), this.speed);
+    this.drawSnake(this.snake);
   }
 
   restartGame() {
@@ -199,20 +268,19 @@ class SnakeGame extends LitElement {
     ];
     this.clear();
     this.drawSnake(this.snake);
+    this.speed = MIN_SPEED;
+    this.direction = 'ArrowUp';
     this.gameOver = false;
-    this.firstUpdated();
+    this.startGame();
   }
 
   override render() {
-    const table = this.makeTable(GRID_SIZE, GRID_SIZE);
     return html`
-      <div>High score: ${this.score}</div>
-      <table ?blurry=${this.gameOver}>
+      <table ?blurry=${!this.gameActive}>
         ${this.makeTable(GRID_SIZE, GRID_SIZE)}
       </table>
-      ${this.gameOver
-        ? html`<div @click=${this.restartGame}>Game Over</div>`
-        : nothing}
+      ${this.gameActive ? this.makeScore() : nothing}
+      ${this.makeMenu()}
     `;
   }
 }
