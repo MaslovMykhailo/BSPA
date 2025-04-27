015ea7fb94b7ff43ae1206047b17e58b552f8bc9
Mykhailo Maslov <maslov.mikhail200@gmail.com>
2025-04-27 13:59:32 +0300

    Add tailwind and shadch componets

--- project/eslint.config.js
+++ project/eslint.config.js
@@ -2,12 +2,13 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, prettierRecommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,6 +19,15 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          semi: false,
+          printWidth: 120,
+          singleQuote: true,
+          trailingComma: 'all',
+        },
+      ],
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-    return "Hello, world!";
+  return (
+    <div className="flex h-screen w-screen items-center justify-center flex-col gap-1">
+      <h1 className="text-3xl font-bold underline">"Hello, world!"</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }
